2fb3837adcc6353ef84cbf3c7c53f92fd8d44617
Ali Cagatay <aliccagatay@gmail.com>
2023-10-08 10:03:11 +0100

    Generated a starter "Hello World" screen on `main.dart`.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(
+              fontSize: 40,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
