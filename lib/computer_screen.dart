cccea8a0c2dd6a97213c73390b9eadcd02f106b4
Ali Cagatay <aliccagatay@gmail.com>
2023-10-11 15:26:41 +0100

    Created the `ComputerScreen` component.

--- lib/computer_screen.dart
+++ lib/computer_screen.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class ComputerScreen extends StatelessWidget {
+  const ComputerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'This is a computer display',
+        style: TextStyle(
+          fontSize: 30.0,
+        ),
+      ),
+    );
+  }
+}
